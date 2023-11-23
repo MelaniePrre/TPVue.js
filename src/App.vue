fbe7005862c7347e9840cf11b3ed402f21f4271c
Mélanie Pierre <melanie.pierre@etu.univ-grenoble-alpes.fr>
2023-11-23 21:34:28 +0100

    Creation du repos

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import composantTodo from "./components/TodoList.vue"
+
+export default {
+  name: "TP Vue.js",
+  components: {
+    composantTodo,
+  },
+};
+
+</script>
+
+
+<template>
+  <h1>Voici ma TodoList</h1>
+  <composantTodo>Mon composant</composantTodo>
+</template>
+
+
+<style> 
+  
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
+
+    <div>
+      <h3 v-if="coloreTitre" :style="{color: 'blue'}">Listing</h3>
+      <input v-model="newTask" placeholder="Nouvelle tâche" />
+      <button @click="addTask">Ajouter</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+
+  data() {
+    return {
+      tasks: [],
+      newTask: "",
+      coloreTitre: true,
+    };
+  },
+
+  methods: {
+    addTask: function () {
+      if (this.newTask !== "") {
+        this.tasks.push(this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
